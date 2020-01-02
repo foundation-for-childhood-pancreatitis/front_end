188261a8b99c1a384657c79640ddd831951be297
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-02 13:50:41 -0500

    navigation for desktop done. Added some styles(which aren't final).

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { GlobalStyles } from "./components/index";
 import DashBoard from "./components/views/DashBoard";
 import LoginPage from "./components/views/LoginPage";
-
+import LandingPage from "./components/landing-page/LandingPage";
 
 function App() {
   return (
     <div>
-    <GlobalStyles />
+      {/* <GlobalStyles />
       <Switch>
-      <Route exact path="/dashboard" component={DashBoard} />
-      <Route exact path="/login" component={LoginPage} />
-      <Redirect to="/login" />
-      </Switch>
+        <Route exact path="/dashboard" component={DashBoard} />
+        <Route exact path="/login" component={LoginPage} />
+        <Redirect to="/login" />
+      </Switch> */}
+      <LandingPage />
     </div>
   );
 }
